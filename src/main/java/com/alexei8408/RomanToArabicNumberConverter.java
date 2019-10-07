335e56317370e827a3a93c2bd0b369e6cd0f9af6
alex <Alexei84-08@mail.ru>
2019-10-07 10:45:05 +0300

    Added maven, roman numeral converter and tests.

---
+++ src/main/java/com/alexei8408/RomanToArabicNumberConverter.java
@@ -0,0 +1,34 @@
+package com.alexei8408;
+
+import java.util.ArrayList;
+
+class RomanToArabicNumberConverter {
+    int convert(String romeValue) {
+        String romeValueIgnoredCase = romeValue.toUpperCase();
+        int arabicNumber = 0;
+        int subtractionRuleNumber
+                = 0;
+
+        ArrayList<String> romeList = new ArrayList<String>();
+        for (int i = 0; i <= 10; i++) {
+            romeList.add(i, "");
+        }
+        romeList.set(1, "I");
+        romeList.set(10, "X");
+        romeList.set(5, "V");
+
+        for (int marker = romeValueIgnoredCase.length(); marker > 0; marker--) {
+            String romanSymbol = romeValueIgnoredCase.substring(marker - 1, marker);
+            int valueRomeNumber = romeList.indexOf(romanSymbol);
+
+            if (valueRomeNumber >= subtractionRuleNumber
+            ) {
+                arabicNumber += valueRomeNumber;
+            } else {
+                arabicNumber -= valueRomeNumber;
+            }
+            subtractionRuleNumber = valueRomeNumber;
+        }
+        return arabicNumber;
+    }
+}
---
+++ src/test/java/com/alexei8408/RomanToArabicNumberConverterTest.java
@@ -0,0 +1,79 @@
+package com.alexei8408;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanToArabicNumberConverterTest {
+
+    private int convertToArabic(String romeValue) {
+        RomanToArabicNumberConverter romanToArabicNumberConverter = new RomanToArabicNumberConverter();
+        return romanToArabicNumberConverter.convert(romeValue);
+    }
+
+    @Test
+    void convert_I_To_1_IgnoredCase() {
+        int arabicNumber = convertToArabic("i");
+        assertEquals(1, arabicNumber);
+    }
+
+    @Test
+    void convert_I_To_1() {
+        int arabicNumber = convertToArabic("I");
+        assertEquals(1, arabicNumber);
+    }
+
+    @Test
+    void convert_II_To_2() {
+        int arabicNumber = convertToArabic("Iv");
+        assertEquals(4, arabicNumber);
+    }
+
+    @Test
+    void convert_III_To_3() {
+        int arabicNumber = convertToArabic("III");
+        assertEquals(3, arabicNumber);
+    }
+
+    @Test
+    void convert_IV_To_4() {
+        int arabicNumber = convertToArabic("IV");
+        assertEquals(4, arabicNumber);
+    }
+
+    @Test
+    void convert_V_To_5() {
+        int arabicNumber = convertToArabic("V");
+        assertEquals(5, arabicNumber);
+    }
+
+    @Test
+    void convert_VI_To_6() {
+        int arabicNumber = convertToArabic("VI");
+        assertEquals(6, arabicNumber);
+    }
+
+    @Test
+    void convert_VII_To_7() {
+        int arabicNumber = convertToArabic("VII");
+        assertEquals(7, arabicNumber);
+    }
+
+    @Test
+    void convert_VIII_To_8() {
+        int arabicNumber = convertToArabic("VIII");
+        assertEquals(8, arabicNumber);
+    }
+
+    @Test
+    void convert_IX_To_9() {
+        int arabicNumber = convertToArabic("IX");
+        assertEquals(9, arabicNumber);
+    }
+
+    @Test
+    void convert_X_To_10() {
+        int arabicNumber = convertToArabic("X");
+        assertEquals(10, arabicNumber);
+    }
+}
