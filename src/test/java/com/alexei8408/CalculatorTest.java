fe443c6b344610f67164a3bcc1985a509995ec97
alex <Alexei84-08@mail.ru>
2019-10-07 16:37:01 +0300

    added Calculator and tests

---
+++ src/test/java/com/alexei8408/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.alexei8408;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    private BigDecimal actual(char operation) {
+        return calculator.calc(5, 7, operation);
+
+    }
+
+    @Test
+    void calc_Num_a_Add_Num_b() {
+        assertEquals(new BigDecimal(12), actual('+'));
+    }
+
+    @Test
+    void calc_Num_a_Subtract_Num_b() {
+        assertEquals(new BigDecimal(-2), actual('-'));
+    }
+
+    @Test
+    void calc_Num_a_Multiply_Num_b() {
+        assertEquals(new BigDecimal(35), actual('*'));
+    }
+
+    @Test
+    void calc_Num_a_Divide_Num_b() {
+        BigDecimal expected = new BigDecimal(5).divide(new BigDecimal(7), 9, BigDecimal.ROUND_CEILING);
+        assertEquals(expected, actual('/'));
+    }
+}
--- src/test/java/com/alexei8408/RomanToArabicNumberConverterTest.java
+++ src/test/java/com/alexei8408/RomanToArabicNumberConverterTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanToArabicNumberConverterTest {
+    private RomanToArabicNumberConverter romanToArabicNumberConverter = new RomanToArabicNumberConverter();
 
     private int convertToArabic(String romeValue) {
-        RomanToArabicNumberConverter romanToArabicNumberConverter = new RomanToArabicNumberConverter();
         return romanToArabicNumberConverter.convert(romeValue);
     }
 
